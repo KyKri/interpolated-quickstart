2e0a7b052a85553bc3629b2d09a40a82b86e7bd1
Kyle Krick <krkrick@gmail.com>
2018-09-23 21:46:33 -0700

    added WidthAndAlignment method

--- Program.cs
+++ Program.cs
@@ -34,11 +34,32 @@ namespace InterpolatedStrings
             Console.WriteLine($"On {date:d}, the price of {item} was {price:C2} per {unit}");
         }
 
+        public static void WidthAndAlignment()
+        {
+            var titles = new Dictionary<string, string>()
+            {
+                ["Doyle, Arthur Conan"] = "Hound of the Baskervilles, The",
+                ["London, Jack"] = "Call of the Wild, The",
+                ["Shakespeare, William"] = "Tempest, The"
+            };
+
+            Console.WriteLine($"[{DateTime.Now,-20:d}] Hour [{DateTime.Now,-10:HH}] [{1063.342,15:N2}] feet");
+            Console.WriteLine("Author and Title List");
+            Console.WriteLine();
+            Console.WriteLine($"|{"Author",25}|{"Title",30}|");
+            foreach (var title in titles)
+            {
+                Console.WriteLine($"|{title.Key,25}|{title.Value,30}|");
+            }
+        }
+
         static void Main(string[] args)
         {
             //Eggplant();
 
             //PrintName();
+
+            WidthAndAlignment();
 
             Console.ReadKey();
         }
